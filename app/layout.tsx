461a091bc7b135a5371ddda56439b5d2e9d406dc
Jinank Thakker <jinankthakker@gmail.com>
2026-04-15 20:31:54 -0400

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import Script from "next/script";
 import { Footer } from "@/components/Footer";
 import { Providers } from "./providers";
 import "./globals.css";
@@ -26,6 +27,19 @@ export default function RootLayout({
       <body
         className={`${jakarta.className} min-h-screen bg-slate-50 antialiased flex flex-col`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HT0DDBS8E3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HT0DDBS8E3');
+            gtag('config', 'AW-18093007265');
+          `}
+        </Script>
         <Providers>
           <div className="flex-1 flex flex-col min-h-0">{children}</div>
           <Footer />
